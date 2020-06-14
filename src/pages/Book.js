df984263c83ecb423fc7d71a495b8baf1bcd29a8
Rafael Barros <rafaelbarros.df@gmail.com>
2020-06-14 12:18:04 -0300

    :construction: Criando o Esqueleto do Aplicativo - Formulário

---
+++ src/pages/Book.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+const Book = () => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.pageTitle}>Inclua seu novo livro...</Text>
+            <TextInput
+                style={styles.input}
+                placeholder='Título'
+            />
+            <TextInput
+                style={styles.input}
+                placeholder='Descrição'
+                multiline={true}
+                numberOfLines={4}
+            />
+            <TouchableOpacity style={styles.cameraButton}>
+                <Icon name='photo-camera' size={18} color='#fff'/>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.saveButton}>
+                <Text style={styles.saveButtonText}>Cadastrar</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.cancelButton}>
+                <Text style={styles.cancelButtonText}>Cancelar</Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 10,    
+    },
+    pageTitle: {
+        textAlign: 'center',
+        fontSize: 16,
+        marginBottom: 20,
+    },
+    input: {
+        fontSize: 16,
+        borderBottomColor: '#f39c12',
+        borderBottomWidth: 1,
+        marginBottom: 10
+    },
+    cameraButton: {
+        backgroundColor: '#f39c12',
+        borderRadius: 50,
+        width: 32,
+        height: 32, 
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'center',
+        marginBottom: 20,
+    },
+    saveButton: {
+        backgroundColor: '#f39c12',
+        alignSelf: 'center',
+        borderRadius: 8,
+        paddingVertical: 8,
+        paddingHorizontal: 20,
+        marginBottom: 20,
+    },
+    saveButtonText: {
+        color: '#fff',
+        fontSize: 16,
+    },
+    cancelButton: {
+        alignSelf: 'center',
+    },
+    cancelButtonText: {
+        color: '#95a5a6'
+    }
+
+});
+
+export default Book;
